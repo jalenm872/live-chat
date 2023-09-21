d5b6268d821bd1e25238e90536271411df242ffc
Jalen Mendoza <54971623+jalenm872@users.noreply.github.com>
2023-09-21 18:30:19 -0400

    insalled react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React 18 Alpha</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
+      
   );
 }
 
